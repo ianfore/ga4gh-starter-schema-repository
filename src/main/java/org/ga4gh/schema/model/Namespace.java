b2be92b46e741bc793892b6c1ec923b22b149d4d
Jonathan Fuerth <jfuerth@gmail.com>
2025-03-08 10:06:02 -0500

    Remove unused latestVersion property from Namespace

--- src/main/java/org/ga4gh/schema/model/Namespace.java
+++ src/main/java/org/ga4gh/schema/model/Namespace.java
@@ -1,33 +1,23 @@
 package org.ga4gh.schema.model;
 
-import java.util.Objects;
-
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonView;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 //@JsonView(SerializeView.Public.class)
 public class Namespace {
 
 	  private String namespaceName;
-	  private String latestVersion;
-	  private String contact_url;
-	  
-	Namespace() {}
-
-	public Namespace(String name, String latestVersion, String contact_url) {
-
-	    this.namespaceName = name;
-	    this.latestVersion = latestVersion;
-    this.contact_url = contact_url;
-  }
-
+	  private String contactUrl;
 
 }
